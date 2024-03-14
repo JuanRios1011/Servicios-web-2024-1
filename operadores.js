5520d043a2e46d47ff4e7dc525b5c850a34a4356
JuanRios1011 <juandavidrios1624@gmail.com>
2024-03-13 21:02:29 -0500

    se agrego operadores.js

---
+++ operadores.js
@@ -0,0 +1,40 @@
+/*
+console.log("operations...")
+let counter = 1000
+//suma
+counter=counter+20
+console.log(counter)
+counter += 30
+console.log(counter)
+//resta
+counter=counter-20
+console.log(counter)
+counter-=30
+console.log(counter)
+//multiplicacion
+counter=counter*3
+console.log(counter)
+counter*=4
+console.log(counter)
+//division
+counter=counter/5
+console.log(counter)
+counter/=4
+console.log(counter)
+//residuo
+counter=counter%3
+console.log(counter)
+counter%=3
+console.log(counter)
+//exponenciacion
+counter=counter**3
+console.log(counter)
+counter**=2
+console.log(counter)
+*/
+
+let counter = "100"
+let age = 100
+
+let resultado1 = counter == age 
+let resultado2 = counter === age

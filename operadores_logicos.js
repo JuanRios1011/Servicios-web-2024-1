45de0ff2267a56e706013c20adf07567d9349587
JuanRios1011 <juandavidrios1624@gmail.com>
2024-03-20 20:58:28 -0500

    cambios

--- operadores_logicos.js
+++ operadores_logicos.js
@@ -1,7 +1,28 @@
 console.log("operadores logicos")
 
 console.log("operador AND &&")
+console.log(false && false)
+console.log(true && false)
+console.log(false && true)
+console.log(true && true)
 
-console.log("operador QR || ")
+console.log("operador OR || ")
+console.log(false || false)
+console.log(true || false)
+console.log(false || true)
+console.log(true || true)
 
 console.log("operador NOT !")
+console.log(!true)
+console.log(!false)
+
+let counter = 10
+console.log(`el valor del counter: ${counter}`)
+
+if (counter>100){
+    console.log(`el valor del counter: ${counter} es mayor a 100`)
+}else{
+    console.log(`el valor del counter: ${counter} es menor que 100`)
+}
+
+counter > 100 ? console.log("counter > 100"): console.log("counter < 100")

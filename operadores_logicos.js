c470be2d222f4adc857cf9a8da7733db9fe8d046
JuanRios1011 <juandavidrios1624@gmail.com>
2024-03-20 20:41:35 -0500

    cambios

--- operadores_logicos.js
+++ operadores_logicos.js
@@ -1,31 +1,7 @@
-const fullName = "Juan David Sánchez Ríos"
+console.log("operadores logicos")
 
-console.log(fullName.length)
+console.log("operador AND &&")
 
-console.log(fullName[1])
+console.log("operador QR || ")
 
-console.log(fullName.includes('Juan'))
-
-console.log(fullName.indexOf('David'))
-
-console.log(fullName.startsWith('Juanda'))
-
-console.log(fullName.endsWith('Ríos'))
-
-console.log(fullName.slice(0,18))
-
-console.log(fullName.slice(5))
-
-console.log(fullName.toUpperCase())
-
-console.log(fullName.toLowerCase())
-
-console.log(fullName.replace('Juan', '🐎'))
-
-console.log(fullName.repeat(2))
-
-console.log("          -juan-           ".trim())
-
-console.log("ju an".split(' '))
-
-
+console.log("operador NOT !")
---
+++ string_metodos.js
@@ -0,0 +1,35 @@
+const fullName = "juan David Sánchez Ríos"
+const firstName="juan"
+
+console.log(fullName.length)
+
+console.log(fullName[1])
+
+console.log(fullName.includes('Juan'))
+
+console.log(fullName.indexOf('David'))
+
+console.log(fullName.startsWith('Juanda'))
+
+console.log(fullName.endsWith('Ríos'))
+
+console.log(fullName.slice(0,18))
+
+console.log(fullName.slice(5))
+
+console.log(fullName.toUpperCase())
+
+console.log(fullName.toLowerCase())
+
+console.log(fullName.replace('Juan', '🐎'))
+
+console.log(fullName.repeat(2))
+
+console.log("          -juan-           ".trim())
+
+console.log("ju an".split(' '))
+
+
+console.log(firstName.replace("juan", "Juan"))
+
+

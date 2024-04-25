d3ecd79e780c3078138390cd225f8eadbd9d6976
JuanRios1011 <juandavidrios1624@gmail.com>
2024-04-24 20:49:54 -0500

    clase 24 abril

---
+++ dowhile.js
@@ -0,0 +1,11 @@
+let counter = 1
+while (counter < 10){
+    console.log(counter)
+    counter++
+}
+
+let isOpen = false
+do {
+console.log(`isOpen = ${isOpen}`)
+} while (isOpen)
+
---
+++ for.js
@@ -0,0 +1,9 @@
+let counter = 0
+while(counter<=100){
+    console.log(`counter = ${counter}`)
+    counter++
+}
+
+for (let i = 0; i<=100; i++){
+    console.log(`counter with for = ${i}`)
+}

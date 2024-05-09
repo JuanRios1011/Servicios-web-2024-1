11235f589c49cd1d8cb2e0b9be0e56a46aad3aa1
JuanRios1011 <juandavidrios1624@gmail.com>
2024-05-08 19:13:35 -0500

    cambios

--- dowhile.js
+++ dowhile.js
@@ -1,11 +1,19 @@
-let counter = 1
-while (counter < 10){
-    console.log(counter)
-    counter++
+let counter = 1;
+while (counter < 10) {
+  console.log(counter);
+  counter++;
 }
 
-let isOpen = false
+let counterTwo = 1;
 do {
-console.log(`isOpen = ${isOpen}`)
-} while (isOpen)
+  console.log(`counter = ${counterTwo}`);
+  counterTwo++;
+} while (counterTwo < 10);
 
+let number = 0;
+do {
+  number = +prompt("ingrese un numero mayor que 100");
+  if (number >= 100) {
+    alert("escribiste el numero correcto");
+  }
+} while (number < 100);
